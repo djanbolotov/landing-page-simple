145ce48eb4a66cf4f4143c64db4ac0bf3539a3e9
Askabek <72981665+djanbolotov16@users.noreply.github.com>
2021-01-12 13:53:31 +0600

    Add files via upload

---
+++ exam/assets/js/script.js
@@ -0,0 +1,7 @@
+//Code for Menu-Burger 
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
