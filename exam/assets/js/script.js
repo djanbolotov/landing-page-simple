485c7d15aafff7bc90fd575d732739a232ba149d
Askabek <72981665+djanbolotov16@users.noreply.github.com>
2022-01-18 12:24:23 +0600

    Delete exam directory

--- exam/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-//Code for Menu-Burger 
-$(document).ready(function(){
-	$('.header__burger').click(function(event){
-		$('.header__burger, .header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
-	});
-});
